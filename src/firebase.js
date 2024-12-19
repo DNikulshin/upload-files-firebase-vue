739c016fc87f1f0eee25f8f6375c15ece31c3c24
Dmitriy <98643483+DNikulshin@users.noreply.github.com>
2024-12-19 15:51:34 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from 'firebase/storage'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const app = initializeApp({
-    apiKey: "AIzaSyByZcIoLuxgSazO2_EqHi3146Bp6rUXcKY",
+    apiKey: "",
     authDomain: "upload-files-app.firebaseapp.com",
     projectId: "upload-files-app",
     storageBucket: "upload-files-app.appspot.com",
-    messagingSenderId: "121597473248",
-    appId: "1:121597473248:web:688b972e139b1d0213bb85"
+    messagingSenderId: "",
+    appId: ""
 })
 
 export const storage = getStorage(app)
